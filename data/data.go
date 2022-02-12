5f2e510e08ea6dcf035a9d1a3806c3e35a5b60cf
= <j.horne2796@gmail.com>
2022-02-12 10:30:30 -0600

    updated help

--- data/data.go
+++ data/data.go
@@ -16,21 +16,10 @@ var UnknownCommand =
 Colors.Red("I'm sorry, I do not understand what you mean. Try 'commands'.\n\n")
 
 var Help =
-Colors.Yellow(`Tenland is a multiplayer text-based game.
-In Tenland, you start out as a ghost that can move around in the endless void
-of empty space. To begin playing you must 'register' and then 'login'. Try
-'help <command>' to get more information on commands. For example, type...
-'help register'.
-Once you're registered, you'll enter the world of Tenland. Tenland is a large
-circular island. There are different resources on different parts of the island.
-For a map of the island resources, type 'map'.
-Tenland itself can be played in a variety of ways. Some players choose to gain
-wealth in the form of Gold (type 'gold') while others choose to do PvP or build.
-Please note that Tenland is very new. There's not a ton of information out yet.
-Stay tuned and feel free to help by being active on the game, in the community
-and by contributing code.
-Try 'commands' for a list of all available commands.
-Type 'begin' to start.`)
+Colors.Yellow(`Tenland is a multiplayer text-based RPG.
+more coming soon...
+
+Type 'help register' and 'help login' to begin.`)
 
 var Begin =
 Colors.Yellow("To begin, you need to register an account with us.\n" +
